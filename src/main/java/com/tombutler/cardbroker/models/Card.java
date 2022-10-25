e61712e09d4403cede46916aee9f96a128b5f58a
Tom Butler <tfbt5987@gmail.com>
2022-10-24 21:51:07 -0400

    card model created

---
+++ src/main/java/com/tombutler/cardbroker/models/Card.java
@@ -0,0 +1,121 @@
+package com.tombutler.cardbroker.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="cards")
+public class Card {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message="Please enter year")
+	private int year;
+	
+	@NotEmpty(message="Please enter player name")
+	private String playerName;
+	
+	@NotEmpty(message="Please enter card description")
+	private String cardDescription;
+	
+	@NotNull(message="Is this card an auto?")
+	private boolean isAuto;
+	
+	@NotNull(message="Is this a rookie card?")
+	private boolean isRookie;
+	
+	@Column(updatable=false)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date createdAt;
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date updatedAt;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="user_id")
+	private User user;
+	
+	public Card( ) {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public String getCardDescription() {
+		return cardDescription;
+	}
+
+	public void setCardDescription(String cardDescription) {
+		this.cardDescription = cardDescription;
+	}
+
+	public boolean isAuto() {
+		return isAuto;
+	}
+
+	public void setAuto(boolean isAuto) {
+		this.isAuto = isAuto;
+	}
+
+	public boolean isRookie() {
+		return isRookie;
+	}
+
+	public void setRookie(boolean isRookie) {
+		this.isRookie = isRookie;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	@PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+	
+}
--- src/main/java/com/tombutler/cardbroker/models/User.java
+++ src/main/java/com/tombutler/cardbroker/models/User.java
@@ -1,12 +1,15 @@
 package com.tombutler.cardbroker.models;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -50,6 +53,9 @@ public class User {
 	private Date createdAt;
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date updatedAt;
+	
+	@OneToMany(mappedBy="user", fetch=FetchType.LAZY)
+	private List<Card> card;
 	
 	public User() {}
 
